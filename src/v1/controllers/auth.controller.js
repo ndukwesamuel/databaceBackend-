cfcf29a946154ef98f434dd580c9f776e6ccdebf
ndukwesamuel <ndukwesamuel23@yahoo.com>
2025-01-28 16:34:50 +0100

    fff

--- src/v1/controllers/auth.controller.js
+++ src/v1/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import asyncWrapper from "../../middlewares/asyncWrapper.js";
 import authService from "../../v1/services/auth.service.js";
 import guarantorModel from "../models/guarantor.model.js";
+import mongoose from "mongoose";
 
 export const register = asyncWrapper(async (req, res, next) => {
   const userData = req.body;
@@ -43,28 +44,6 @@ export const resetPassword = asyncWrapper(async (req, res, next) => {
   const result = await authService.resetPassword({ email, otp, password });
   res.status(200).json(result);
 });
-
-// export const createGuarantor = async (req, res) => {
-//   try {
-//     const { guarantorId, signature, photo, dojah } = req.body;
-
-//     // Create a new Guarantor document
-//     const newGuarantor = new guarantorModel({
-//       guarantorId,
-//       signature,
-//       photo,
-//       dojah,
-//     });
-
-//     // Save the document to the database
-//     await newGuarantor.save();
-
-//     // Return the created document
-//     res.status(201).json(newGuarantor);
-//   } catch (error) {
-//     res.status(500).json({ message: error.message });
-//   }
-// };
 
 export const createGuarantor = async (req, res) => {
   try {
@@ -152,10 +131,6 @@ export const createGuarantor = async (req, res) => {
 //   }
 // };
 
-import mongoose from "mongoose";
-import Guarantor from "../models/Guarantor"; // Import Guarantor model
-import User from "../models/User"; // Import User model
-
 export const Guarantor_webhook = async (req, res) => {
   const session = await mongoose.startSession();
 
@@ -225,14 +200,14 @@ export const Guarantor_webhook = async (req, res) => {
 
 export const getGuarantorById = async (req, res) => {
   const { id } = req.params;
+
+  console.log({
+    ggg: id,
+  });
+
   try {
-    console.log({
-      ggg: id,
-    });
-
-    const guarantor = await guarantorModel.find({ guarantorId: id }); // Fetch guarantor by ID
-
-    res.status(200).json({ success: true, data: guarantor });
+    // const guarantor = await guarantorModel.find({ guarantorId: id }); // Fetch guarantor by ID
+    // res.status(200).json({ success: true, data: guarantor });
     // res.status(200).json({ success: true, data: id });
   } catch (error) {
     res.status(500).json({
